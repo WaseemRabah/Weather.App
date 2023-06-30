3d9620b9f7330015fcf56f2337133f75dd882db9
WaseemRabah <>
2023-06-30 23:36:44 +0000

    Add fetchweather method

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,6 @@
 // API key for accessing the OpenWeatherMap API
 let weather = {
-    apikey: "8abf7d32ed7a60b78ee3c1903714fa3e",;
+    apikey: "8abf7d32ed7a60b78ee3c1903714fa3e",
+    //Define the fetchweather method
+    fetchWeather: function (city) { }
 };
