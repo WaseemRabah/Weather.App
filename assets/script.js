375e536ec7a2b4136a0c05e85f0db4298d4983a1
WaseemRabah <>
2023-07-01 00:19:37 +0000

    Add an http get request to the OpenWeatherMap apiI to fetch weather data for a specific city.

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,16 @@
 // API key for accessing the OpenWeatherMap API
 let weather = {
     apikey: "8abf7d32ed7a60b78ee3c1903714fa3e",
-    //Define the fetchweather method
-    fetchWeather: function (city) { }
+//Define the fetchweather method
+    fetchWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q="
+            + city
+            + "&units=metric&appid="
+            + this.apikey
+// Convert the response to JSON format
+        ).then((response) => response.json())
+// Pass the JSON data to the displayWeather method
+        .then((data) => this.displayWeather(data));
+    }
 };
