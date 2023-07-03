ea5ba872dd480edc62220103fe4b930acbd6e6d6
WaseemRabah <>
2023-07-03 06:19:17 +0000

    Add orderd code to make it more readable and clear

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,15 @@
+const cityElement = document.querySelector(".city");
+const iconElement = document.querySelector(".icon");
+const descriptionElement = document.querySelector(".description");
+const tempElement = document.querySelector(".temp");
+const humidityElement = document.querySelector(".humidity");
+const windElement = document.querySelector(".wind");
+const bodyElement = document.body;
+const searchBtn = document.querySelector(".search button");
+const searchBar = document.querySelector(".search-bar");
+
+
+
 let weather = {
     apikey: "8abf7d32ed7a60b78ee3c1903714fa3e",
     fetchWeather: function (city) {
@@ -7,7 +19,7 @@ let weather = {
             + "&units=metric&appid="
             + this.apikey
         ).then((response) => response.json())
-        .then((data) => this.displayWeather(data));
+            .then((data) => this.displayWeather(data));
     },
     displayWeather: function (data) {
         const { name } = data;
@@ -15,27 +27,27 @@ let weather = {
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
         console.log(name, icon, description, temp, humidity, speed);
-        document.querySelector(".city").innerText = "Weather in " + name;
-        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
-        document.querySelector(".description").innerText = description;
-        document.querySelector(".temp").innerText = temp + "°C";
-        document.querySelector(".humidity").innerText = "Humidity" + " " + humidity + "%";
-        document.querySelector(".wind").innerText = "Wind Speed" + " " + speed + "km/h";
-        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')";
+        cityElement.innerText = `Weather in ${name}`;
+        iconElement.src = `https://openweathermap.org/img/wn/${icon}.png`;
+        descriptionElement.innerText = description;
+        tempElement.innerText = `${temp}°C`;
+        humidityElement.innerText = `Humidity ${humidity}%`;
+        windElement.innerText = `Wind Speed ${speed}km/h`;
+        bodyElement.style.backgroundImage = `url('https://source.unsplash.com/1600x900/?${name}')`;
     },
 
     search: function () {
-        this.fetchWeather(document.querySelector(".search-bar").value);
+        this.fetchWeather(searchBar.value);
     }
 };
 
-document.querySelector(".search button").addEventListener("click", function () {
+searchBtn.addEventListener("click", function () {
     weather.search();
 });
-document.querySelector(".search-bar").addEventListener("keyup", function (event) {
-        if (event.key == "Enter") {
-            weather.search();
-        }
-    });
-    
+searchBar.addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
+});
+
 weather.fetchWeather("Berlin");
